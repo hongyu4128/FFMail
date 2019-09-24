77545e8153754ebb6d4d1a9bb92fc1aa3ce2a307
Christopher.H <hongyu_4128@foxmail.com>
2019-09-24 19:15:10 +0800

    增加一些其他设置

---
+++ FFMail-Basic/basic/src/main/java/com/yhhy/FFMailBasic/basic/BasicApp.java
@@ -0,0 +1,29 @@
+package com.yhhy.FFMailBasic.basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.alibaba.dubbo.spring.boot.annotation.EnableDubboConfiguration;
+import com.yhhy.FFMailBasic.basic.common.SpringContext;
+
+/**
+ * Hello world!
+ *
+ */
+@RestController
+@Configuration
+@EnableAutoConfiguration
+@EnableDubboConfiguration
+@ComponentScan
+@SpringBootApplication
+public class BasicApp {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = SpringApplication.run(BasicApp.class, args);
+        SpringContext.setApplicationContext(applicationContext);
+    }
+}
--- FFMail-Basic/basic/src/test/java/com/yhhy/FFMailBasic/basic/BasicAppTest.java
+++ FFMail-Basic/basic/src/test/java/com/yhhy/FFMailBasic/basic/BasicAppTest.java
@@ -7,7 +7,7 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
+public class BasicAppTest 
     extends TestCase
 {
     /**
@@ -15,7 +15,7 @@ public class AppTest
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
+    public BasicAppTest( String testName )
     {
         super( testName );
     }
@@ -25,7 +25,7 @@ public class AppTest
      */
     public static Test suite()
     {
-        return new TestSuite( AppTest.class );
+        return new TestSuite( BasicAppTest.class );
     }
 
     /**
